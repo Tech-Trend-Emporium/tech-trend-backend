afb3df7f830d2fcb41968c666d6bc29bc6326aaf
Juan Sebastián Dosman <sebas_dosman@Juans-MacBook.local>
2025-11-24 21:02:30 -0500

    fix: 🐛 CORS enabled for front-end deployment
    Add CORS for front-end deployment

--- TechTrendEmporium/Program.cs
+++ TechTrendEmporium/Program.cs
@@ -34,7 +34,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:3000",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "https://main.d3pj42gn592v1q.amplifyapp.com"
             )
             .AllowAnyMethod()
             .AllowAnyHeader()
